8ea1c5b4780e9cd4634aa20d0c14860170160587
José Medeiros <nicruo@gmail.com>
2024-04-29 16:40:23 +0100

    Added About page; Fixed responsive tables and layout;

---
+++ src/Pages/About.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GitHubRepoSecrets.Pages
+{
+    public class AboutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
